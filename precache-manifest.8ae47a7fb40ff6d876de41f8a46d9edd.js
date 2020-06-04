507e69f1b6fccd9d93463fd9c54f7293d1e29821
ifycode <marychord@gmail.com>
2020-06-03 22:21:20 -0800

    Updates

---
+++ precache-manifest.8ae47a7fb40ff6d876de41f8a46d9edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e96c7974cae5d81aace73d1c5ca198c",
+    "url": "/React-Movie-Search-App/index.html"
+  },
+  {
+    "revision": "e5c3ad2d8654623f75e8",
+    "url": "/React-Movie-Search-App/static/css/main.7d7545a9.chunk.css"
+  },
+  {
+    "revision": "c0ec41917beff304cbca",
+    "url": "/React-Movie-Search-App/static/js/2.692b69f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Movie-Search-App/static/js/2.692b69f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c3ad2d8654623f75e8",
+    "url": "/React-Movie-Search-App/static/js/main.a07385ff.chunk.js"
+  },
+  {
+    "revision": "aed6ac4711121d691da2",
+    "url": "/React-Movie-Search-App/static/js/runtime-main.3ed1b80b.js"
+  }
+]);
