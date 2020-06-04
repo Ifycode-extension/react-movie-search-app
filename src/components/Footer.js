afa235e6d40416ad4f075ad675401f4def1b7810
ifycode <marychord@gmail.com>
2020-06-04 09:08:40 -0800

    update github repo link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Footer() {
     return(
         <footer>
-            <p className='footer-text'>&copy;2020 Movie Search App</p>
+            <p className='footer-text'>
+                <span className='copyright'>&copy;2020 Movie Search App</span> 
+                <a className='link-to-repo' href='https://github.com/Ifycode/React-Movie-Search-App' target='_blank' rel='noopener noreferrer'>Repo</a>
+            </p>
         </footer>
     );
 }
